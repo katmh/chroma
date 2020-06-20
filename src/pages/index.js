14e98f87c842705c4c2a79ddd577f2d3e5e34bfb
Katherine Huang <hello@katmh.com>
2020-06-20 17:48:22 -0400

    Add article gallery to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,54 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import Container from "../components/container"
 import Shelf from "../components/shelf"
+import { Link } from "gatsby"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <Shelf />
     <Container>
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div sx={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
+      <section
+        sx={{
+          display: "grid", 
+          gridTemplateColumns: "repeat(3, 1fr)",
+          gridGap: 4,
+        }}
+      >
+      {data.allMarkdownRemark.edges.map(edge => (
+        <article>
+          <Link to={'p/' + edge.node.frontmatter.path}>
+            <h1>{edge.node.frontmatter.title}</h1>
+          </Link>
+          <div dangerouslySetInnerHTML={{ __html: edge.node.excerpt }} />
+        </article>
+      ))}
+      </section>
     </Container>
   </Layout>
 )
+
+export const query = graphql`
+{
+  allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, filter: {fileAbsolutePath: {}}) {
+    edges {
+      node {
+        frontmatter {
+          path
+          author
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        excerpt(pruneLength: 200)
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
